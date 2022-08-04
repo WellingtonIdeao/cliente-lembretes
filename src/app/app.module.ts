6b3a9f3bf71918c4733b605ea7ea459fd6c51648
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-04 13:30:10 -0300

    Added form-lembrete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ErrorMsgComponent } from './shared/error-msg/error-msg.component';
 import { ListaLembreteComponent } from './pages/lista-lembrete/lista-lembrete.component';
+import { FormLembreteComponent } from './shared/form-lembrete/form-lembrete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorMsgComponent,
-    ListaLembreteComponent
+    ListaLembreteComponent,
+    FormLembreteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
