c2f3efe9b6dadac63ae8ab21bc46659aec650424
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-03 22:25:24 -0300

    Error message component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ErrorMsgComponent } from './shared/error-msg/error-msg.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  
+
   title = 'CRUD de lembretes com o Angular';
+  @ViewChild(ErrorMsgComponent) errorMsgComponent!: ErrorMsgComponent;
+  
+  
+  ngAfterViewInit() {
+    setTimeout(()=> this.errorMsgComponent.setError("Mensagem de erro!"));
+  }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorMsgComponent } from './shared/error-msg/error-msg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorMsgComponent
   ],
   imports: [
     BrowserModule,
